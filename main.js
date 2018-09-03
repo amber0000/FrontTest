02faffbb1c9ad1cb2a2dc41b4f99161cda23d4b3
amber0000 <akdlwps0@gmail.com>
2018-09-03 14:53:04 +0900

    핸손테이블 callback 이벤트 테스트 ( 차트데이터 select 시 활용 )

--- main.js
+++ main.js
@@ -8,53 +8,74 @@ define('main', ['backtesting'], function (backtesting) {
 
 		$("#button01").click(function () {
 			var obj = {
-			data : [
-				{
-					id: 1,
-					flag: 'EUR',
-					currencyCode: 'EUR',
-					currency: 'Euro',
-					level: 0.9033,
-					units: 'EUR / USD',
-					asOf: '08/19/2018',
-					onedChng: 0.0026
-				}
-			],
-			columns: [
-				{
-					data: 'id',
-					type: 'numeric',
-					width: 40
-				},
-				{
-					data: 'flag'
-				},
-				{
-					data: 'currencyCode',
-					type: 'text'
-				},
-				{
-					data: 'currency',
-					type: 'text'
-				},
-				{
-					data: 'level',
-					type: 'numeric',
-					numericFormat: {
-					pattern: '0.0000'
+				data : [
+					{
+						id: 1,
+						flag: 'EUR',
+						currencyCode: 'EUR',
+						currency: 'Euro',
+						level: 0.9033,
+						units: 'EUR / USD',
+						asOf: '08/19/2018',
+						onedChng: 0.0026
 					}
-				}
-			],
-			colHeaders: [
-				'날짜',
-				'롱/숏',
-				'진입가',
-				'청산가',
-				'계약수'
-			]
-		}
-		backtesting.sheet.updateSettings(obj);
-		return false;
+				],
+				columns: [
+					{
+						data: 'id',
+						type: 'numeric',
+						width: 40
+					},
+					{
+						data: 'flag'
+					},
+					{
+						data: 'currencyCode',
+						type: 'text'
+					},
+					{
+						data: 'currency',
+						type: 'text'
+					},
+					{
+						data: 'level',
+						type: 'numeric',
+						numericFormat: {
+						pattern: '0.0000'
+						}
+					}
+				],
+				colHeaders: [
+					'날짜',
+					'롱/숏',
+					'진입가',
+					'청산가',
+					'계약수'
+				]
+			}
+			backtesting.sheet.updateSettings(obj);
+			return false;
+		});
+
+		$("#button02").click(function () {
+			var obj = {
+				afterSelectionByProp: function(row, column, row2, column2, preventScrolling, selectionLayerLevel){
+					console.log(row);
+					console.log(column);
+					console.log(row2);
+					console.log(column2);
+					console.log(preventScrolling);
+					console.log(selectionLayerLevel);
+					console.log("---------------------");
+	
+				},
+				afterDeselect: function(){
+					console.log('afterDeselect');
+				},
+			}
+
+			backtesting.sheet.updateSettings(obj);
+			return false;
 		});
 	});
 
